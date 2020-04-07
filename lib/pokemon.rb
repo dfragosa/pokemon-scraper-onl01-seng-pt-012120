2b894c0133d83f022ccfda7b2148896ddbc9381f
Derric Fragosa <fderric@gmail.com>
2020-04-07 18:46:06 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,7 @@ class Pokemon
     VALUES (?,?,?)
     SQL
     @db.execute(sql, self.name, self.type, self.db)
-    @id = @db.execute("SELECT last_insert_rowid() FROM pokeman")
+    @id = @db.execute("SELECT last_insert_rowid() FROM pokeman")[0][0]
   end
 
 end
