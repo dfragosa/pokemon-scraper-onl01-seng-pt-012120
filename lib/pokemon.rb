302096becfb84b32e6cdd84433fd7fe6828d1b9f
Derric Fragosa <fderric@gmail.com>
2020-04-07 19:06:35 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,7 +10,7 @@ class Pokemon
   end
   
   def self.save(name, type, db)
-    @db.execute("INSERT INTO pokeman(name, type, db) VALES ")
+    @db.execute("INSERT INTO pokeman(name, type, db) VALUES (?,?,?)")
     @id = @db.execute("SELECT last_insert_rowid() FROM pokeman")[0][0]
   end
 
